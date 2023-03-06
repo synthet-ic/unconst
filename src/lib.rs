eb918ce6affea50ebefb28cefb32af2e7012d49a
root <rnarkkx@gmail.com>
2023-03-06 16:47:19 +0000

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,20 @@
 
 extern crate alloc;
 
-use alloc::string::ToString;
+use alloc::{
+    string::ToString,
+    vec::Vec
+};
 
 use proc_macro::TokenStream;
 use proc_macro2::Ident;
 use quote::quote;
-use syn::{parse, Item, ImplItem, TraitItem};
+use syn::{
+    parse, Item, ImplItem, TraitItem, Attribute, Generics, GenericParam,
+    TypeParamBound, TraitBound, Signature, WherePredicate,
+    punctuated::Punctuated,
+    token::Add
+};
 
 #[cfg(feature = "const")]
 #[proc_macro_attribute]
@@ -20,46 +28,97 @@ pub fn unconst(_attr: TokenStream, item: TokenStream) -> TokenStream {
 pub fn unconst(_attr: TokenStream, item: TokenStream) -> TokenStream {
     match parse::<Item>(item).unwrap() {
         Item::Fn(mut r#fn) => {
-            r#fn.sig.constness = None;
+            unconst_sig(&mut r#fn.sig);
             return quote!(#r#fn).into();
         }
         Item::Enum(mut r#enum) => {
-            for attr in r#enum.attrs.iter_mut() {
-                let mut segment = attr.path.segments.first_mut().unwrap();
-                if segment.ident.to_string() == "derive_const" {
-                    segment.ident = Ident::new("derive", segment.ident.span());
-                }
-            }
+            unconst_attrs(&mut r#enum.attrs);
+            unconst_generics(&mut r#enum.generics);
             return quote!(#r#enum).into()
         }
         Item::Impl(mut r#impl) => {
             for item in r#impl.items.iter_mut() {
                 match item {
-                    ImplItem::Method(method) => method.sig.constness = None,
+                    ImplItem::Method(method) => unconst_sig(&mut r#method.sig),
                     _ => continue
                 };
             }
+            unconst_generics(&mut r#impl.generics);
             return quote!(#r#impl).into()
         }
         Item::Struct(mut r#struct) => {
-            for attr in r#struct.attrs.iter_mut() {
-                let mut segment = attr.path.segments.first_mut().unwrap();
-                if segment.ident.to_string() == "derive_const" {
-                    segment.ident = Ident::new("derive", segment.ident.span());
-                }
-            }
+            unconst_attrs(&mut r#struct.attrs);
+            unconst_generics(&mut r#struct.generics);
             return quote!(#r#struct).into()
         }
         Item::Trait(mut r#trait) => {
             for item in r#trait.items.iter_mut() {
                 match item {
-                    TraitItem::Method(method) => method.sig.constness = None,
+                    TraitItem::Method(method) => unconst_sig(&mut r#method.sig),
                     _ => continue
                 };
+            }
+            unconst_generics(&mut r#trait.generics);
+            for supertrait in r#trait.supertraits.iter_mut() {
+                match supertrait {
+                    TypeParamBound::Trait(bound) => unconst_trait_bounds(bound),
+                    _ => continue
+                }
             }
             return quote!(#r#trait).into()
         }
         Item::Verbatim(_tt) => unimplemented!(),
-        _ => panic!("Input is neither a function, a trait nor an impl")
+        _ => panic!("Input must be one of function/enum/struct/trait/impl")
+    }
+}
+
+fn unconst_attrs(attrs: &mut Vec<Attribute>) {
+    for attr in attrs.iter_mut() {
+        let mut segment = attr.path.segments.first_mut().unwrap();
+        if segment.ident.to_string() == "derive_const" {
+            segment.ident = Ident::new("derive", segment.ident.span());
+        }
+    }
+}
+
+fn unconst_sig(signature: &mut Signature) {
+    signature.constness = None;
+    unconst_generics(&mut signature.generics);
+}
+
+fn unconst_generics(generics: &mut Generics) {
+    for param in generics.params.iter_mut() {
+        match param {
+            GenericParam::Type(param) => unconst_bounds(&mut param.bounds),
+            _ => continue
+        }
+    }
+    if let Some(r#where) = generics.where_clause.as_mut() {
+        for predicate in r#where.predicates.iter_mut() {
+            match predicate {
+                WherePredicate::Type(predicate) => {
+                    unconst_bounds(&mut predicate.bounds);
+                },
+                _ => continue
+            }
+        }
+    }
+}
+
+fn unconst_bounds(bounds: &mut Punctuated<TypeParamBound, Add>) {
+    for bound in bounds.iter_mut() {
+        match bound {
+            TypeParamBound::Trait(bound) => {
+                unconst_trait_bounds(bound);
+            },
+            _ => continue
+        }
+    }
+}
+
+fn unconst_trait_bounds(trait_bound: &mut TraitBound) {
+    let mut segment = trait_bound.path.segments.first_mut().unwrap();
+    if let Some(ident) = segment.ident.to_string().strip_prefix("~const ") {
+        segment.ident = Ident::new(ident, segment.ident.span());
     }
 }
