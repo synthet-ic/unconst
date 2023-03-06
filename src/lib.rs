9c74b17e91d94e268bb17422bd59a1313866165c
root <rnarkkx@gmail.com>
2023-03-06 14:45:10 +0000

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 #![no_std]
 
+extern crate alloc;
+
+use alloc::string::ToString;
+
 use proc_macro::TokenStream;
+use proc_macro2::Ident;
 use quote::quote;
 use syn::{parse, Item, ImplItem, TraitItem};
 
@@ -11,6 +16,15 @@ pub fn unconst(_attr: TokenStream, item: TokenStream) -> TokenStream {
             r#fn.sig.constness = None;
             return quote!(#r#fn).into();
         }
+        Item::Enum(mut r#enum) => {
+            for attr in r#enum.attrs.iter_mut() {
+                let mut segment = attr.path.segments.first_mut().unwrap();
+                if segment.ident.to_string() == "derive_const" {
+                    segment.ident = Ident::new("derive", segment.ident.span());
+                }
+            }
+            return quote!(#r#enum).into()
+        }
         Item::Impl(mut r#impl) => {
             for item in r#impl.items.iter_mut() {
                 match item {
@@ -19,6 +33,15 @@ pub fn unconst(_attr: TokenStream, item: TokenStream) -> TokenStream {
                 };
             }
             return quote!(#r#impl).into()
+        }
+        Item::Struct(mut r#struct) => {
+            for attr in r#struct.attrs.iter_mut() {
+                let mut segment = attr.path.segments.first_mut().unwrap();
+                if segment.ident.to_string() == "derive_const" {
+                    segment.ident = Ident::new("derive", segment.ident.span());
+                }
+            }
+            return quote!(#r#struct).into()
         }
         Item::Trait(mut r#trait) => {
             for item in r#trait.items.iter_mut() {
